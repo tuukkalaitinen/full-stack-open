6c0446391e91059d05739a7c112df7b27ee276e8
Tuukka Laitinen <work.ictlaitinen@gmail.com>
2023-12-13 18:40:26 +0200

    Exercise 2.2: Course information step7 - implemented

--- osa2/kurssitiedot/kurssitiedot/src/Course.jsx
+++ osa2/kurssitiedot/kurssitiedot/src/Course.jsx
@@ -2,10 +2,16 @@ import Header from "./Header.jsx";
 import Content from "./Content.jsx";
 
 const Course = (props) => {
+
+    const totalNrOfExercises = props.course.parts.reduce(function (accumulator, part) {
+        return accumulator + part.exercises;
+    }, 0);
+
     return (
         <div>
             <Header courseName={props.course.name}></Header>
             <Content parts={props.course.parts}></Content>
+            <p><b>total of {totalNrOfExercises} exercises</b></p>
         </div>
     )
 }
