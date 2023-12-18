1247ed771f5657a090b83ced92bad8bd1f7c0daf
Tuukka Laitinen <work.ictlaitinen@gmail.com>
2023-12-18 08:11:19 +0200

    Exercise 2.6: The Phonebook Step1 - implemented

---
+++ osa2/puhelinluettelo/puhelinluettelo/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+const App = () => {
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas' }
+    ])
+    const [newName, setNewName] = useState('')
+
+    const handleClick = (event) => {
+        setPersons(persons.concat({name: newName}))
+        setNewName('')
+    }
+
+    const handleOnSubmit = (event) => {
+        console.log('attempted submit stopped!');
+        event.preventDefault()
+    }
+
+    const handleOnChange = (event) => {
+        setNewName(event.target.value)
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={handleOnSubmit}>
+                <div>
+                    name: <input value={newName} onChange={handleOnChange} />
+                </div>
+                <div>
+                    <button type="button" onClick={handleClick}>add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            {persons.map(person => {
+                return (
+                    <div>{person.name}</div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default App
